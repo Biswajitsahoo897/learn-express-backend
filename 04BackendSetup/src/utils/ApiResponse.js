ce931d55bd7db7692eaee8c7fb583a616f850be4
Biswajitsahoo897 <biswajitsahookunu2005@gmail.com>
2025-07-11 15:51:24 +0530

    Renamed Main Folder

--- 04BackendSetup/src/utils/ApiResponse.js
+++
@@ -1,8 +0,0 @@
-class ApiResponse{
-    constructor(stausCode,data,message="Success"){
-        this.stausCode=stausCode
-        this.data=data
-        this.message=message
-        this.success=stausCode<400
-    }
-}
